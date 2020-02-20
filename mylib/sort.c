66c8992cfd34eac1bf9dbd59a58305e27ea56b2b
Biswayan-Roy <prithviroy91@gmail.com>
2020-02-20 22:58:21 +0530

    mylib

---
+++ mylib/sort.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include "sort.h"
+
+void bubble_sort(int arr[], int l) {
+    int i;
+    for (i = 0; i < l; i++) {
+        printf("\n %d", arr[i]);
+    }
+}
---
+++ mylib/sort.h
@@ -0,0 +1,10 @@
+/*
+ * description: header file for 
+ * */
+
+#ifndef SORT_H__
+#define SORT_H__
+
+void bubble_sort(int [], int);
+
+#endif // SORT_H__
