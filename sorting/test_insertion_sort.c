ee2f281d2268745b867a03829f1b7d341be4d41d
Biswayan-Roy <prithviroy91@gmail.com>
2020-02-21 06:38:36 +0530

    add temp

--- sorting/test_insertion_sort.c
+++ sorting/test_insertion_sort.c
@@ -2,11 +2,11 @@
 #include "../mylib/sort.h"
 
 int main(void) {
-    // test git2
+    // test git3
     printf("Hello, World!\n");
     int a[] = {1, 2, 3};
 
-    bubble_sort(a, 3);
+    insertion_sort(a, 3);
 
     for (int i = 0; i <= 5; i++) {
         printf("\n %d", i);
