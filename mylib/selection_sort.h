67a47329b0aa9db188caa5135be642824193d9c0
Biswayan-Roy <prithviroy91@gmail.com>
2020-02-28 08:17:39 +0530

    selction header

---
+++ mylib/selection_sort.h
@@ -0,0 +1,10 @@
+//
+// Created by asinha on 2/28/20.
+//
+
+#ifndef C_PROGRAMMING_SELECTION_SORT_H
+#define C_PROGRAMMING_SELECTION_SORT_H
+
+void selection_sort(int[], int);
+
+#endif //C_PROGRAMMING_SELECTION_SORT_H
